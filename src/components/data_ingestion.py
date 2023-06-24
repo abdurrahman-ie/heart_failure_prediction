5b7f76569a26b2910ed81f0ed50a272c32f36800
AbdurRahmanIE <abdur114@gmail.com>
2023-06-24 15:55:20 +0600

    update data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,22 +11,34 @@ from src.exception import CustomException
 from src.logger import logging
 
 
+
+'''
+This code performs the data ingestion process by reading a CSV file, 
+splitting the data into train and test sets, and saving them in specified 
+locations. It also logs the steps and handles custom exceptions.
+
+using "@dataclass" helps to directly use class variable
+for more info - https://docs.python.org/3/library/dataclasses.html
+
+To store all the output files in "artifacts" folder: =>
+DataIngestionConfig class is used to store the paths of different 
+data files related to data ingestion
+'''
+
 @dataclass
-# using "@dataclass" helps to directly use class variable
-# for more info - https://docs.python.org/3/library/dataclasses.html
-
-# To store all the output files in "artifacts" folder: =>
-# DataIngestionConfig class is used to store the paths of different data files related to data ingestion 
-
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', "train.csv")
     test_data_path: str=os.path.join('artifacts', "test.csv")
     raw_data_path: str=os.path.join('artifacts', "data.csv")
     '''
-    The above 3 (train_data_path, test_data_path, raw_data_path) are class variables within the DataIngestionConfig class.
+    The above 3 (train_data_path, test_data_path, raw_data_path) are class 
+    variables within the DataIngestionConfig class.
     Each variable represents a data path and variable type is string (str)
 
-    "os.path.join()" function is used to join the directory name ('artifacts') with the respective file name ('train.csv', 'test.csv', 'data.csv'). This ensures that the paths are constructed correctly, taking into account the operating system's file path conventions.
+    "os.path.join()" function is used to join the directory name ('artifacts') 
+    with the respective file name ('train.csv', 'test.csv', 'data.csv'). 
+    This ensures that the paths are constructed correctly, taking into account 
+    the operating system's file path conventions.
     '''
 
 class DataIngestion:
@@ -39,7 +51,7 @@ class DataIngestion:
 
         try:
             '''
-            Here the data read from csv file, but it can read from any server.
+            Data read from csv file and store in a data frame "df", but it can read from any server.
             '''
             df=pd.read_csv('data\heart.csv')
             logging.info("Read data set as data frame.")
@@ -47,10 +59,20 @@ class DataIngestion:
             # Create the artifacts folder to store csv file
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
+            
+            # Convert the data frame "df" into csv file
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
+            '''
+            Split the data set into train & test. For more sure we use "logging.info(") 
+            to ensure program works correctly.
+            '''
+            logging.info("Train test split initiated")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
+            '''
+            Save the splitted train & test file into artifac folder as csv file.
+            '''
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
 
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
